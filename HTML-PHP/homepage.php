663c8d9f275c1f7943fff148a18ed7da6e92b67d
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-04-21 19:35:22 +0300

    Add navbar, fix button

---
+++ HTML-PHP/homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="../CSS/homepage-style.css">
+</head>
+<body>
+    <?php include 'main.php';?>
+    <div class="content">
+        <div class="welcome-text">
+            <h2>Welcome, user</h2>
+            <p>Your next shift starts in [days] and [hours]</p>
+        </div>
+        <div class="btnview">
+<!--            <a href="#">View full schedule ></a>-->
+            <button>View full schedule ></button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ HTML-PHP/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../CSS/main.css">
+    <title>Main</title>
+</head>
+<body>
+    <div class="navbar">
+        <a class="active" href="#"><i class="fa fa-fw fa-home"></i></a>
+        <a href="#"><i class="fa fa-calendar"></i></a>
+        <a href="#"><i class="fa fa-fw fa-user"></i></a>
+    </div>
+
+</body>
+</html>
