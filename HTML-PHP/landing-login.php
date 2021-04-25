d1a5464e543e3bf868231a2986f8edb79f7262cf
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-04-25 21:31:57 +0300

    Add login form as landing page

---
+++ HTML-PHP/landing-login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login page</title>
+    <link rel="stylesheet" href="../CSS/landing-login-styles.css">
+
+</head>
+<body>
+    <div class="main-parent">
+        <div class="main">
+            <p class="login-title" >Log in</p>
+
+            <form class="loginForm">
+                <input class="username" type="text" align="center" placeholder="Username...">
+                <input class="password" type="password" align="center" placeholder="Password...">
+                <a class="submit">Sign in</a>
+                <p class="forgot"><a href="#">Forgot Password?</p>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
+
+
