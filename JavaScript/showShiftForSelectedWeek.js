10f2c9ac65a2dba1806bf267037dafb102d1ea8b
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-06-14 15:57:33 +0200

    selecting weeks in schedule with arrows

--- JavaScript/showShiftForSelectedWeek.js
+++ JavaScript/showShiftForSelectedWeek.js
@@ -1,7 +1,8 @@
+/*
 $(document).ready(function(){
     $("#weeks").on('change', function postinput(){
         var selectedWeek = $(this).val();
         xhttp.open("GET", "../Handling/scheduleHandling.php?selectedWeek=" + selectedWeek, true);
         xhttp.send();
     });
-});
+});*/
