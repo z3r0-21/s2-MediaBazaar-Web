9ece976a5a840b745a14c02c2a5720ad10e196e3
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-06-18 20:38:59 +0200

    Fix some js console warnings

--- HTML-PHP/holidayLeaveRequest.php
+++ HTML-PHP/holidayLeaveRequest.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['loggedUserId']))
 
     <link rel="stylesheet" href="../CSS/holidayLeaveRequest.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
+    <!--<link rel="stylesheet" href="/resources/demos/style.css">-->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
